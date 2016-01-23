9fc7e1562756ce19548a9923b52777a7cc4f0f53
Phil <birkal@outlook.com>
2016-01-22 19:53:40 -0800

    Hello World

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import Rx from 'rx';
 import Cycle from '@cycle/core';
 import {makeDOMDriver, label, input, h1, hr, div} from '@cycle/dom';
 
-function main() {
+function main(sources) {
+  const inputEv$ = sources.DOM.select('.field').events('input');
+  const name$ = inputEv$.map(ev => ev.target.value).startWith('World');
   return {
-    DOM: Rx.Observable.of(
-      div('hello world!')
+    DOM: name$.map(name =>
+      Rx.Observable.of(
+        div([
+          label('Name:'),
+          input('.field', {type: 'text', value: name}),
+          hr(),
+          h1(`Hello ${name}!`)
+        ])
+      )
     )
   };
 }
