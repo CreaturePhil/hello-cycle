57f9633c2e30dc84c6bafa45f2ecac4ac93748a4
Phil <birkal@outlook.com>
2016-01-23 19:47:10 -0800

    mvi

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import Rx from 'rx';
 import Cycle from '@cycle/core';
 import {makeDOMDriver, div, input, label, h2} from '@cycle/dom';
 
-function main(sources) {
-  const changeWeight$ = sources.DOM.select('.weight').events('input')
+function intent(DOMSource) {
+  const changeWeight$ = DOMSource.select('.weight').events('input')
     .map(ev => ev.target.value);
 
-  const changeHeight$ = sources.DOM.select('.height').events('input')
+  const changeHeight$ = DOMSource.select('.height').events('input')
     .map(ev => ev.target.value);
 
+  return {changeWeight$, changeHeight$};
+}
+
+function model(changeWeight$, changeHeight$) {
   const state$ = Rx.Observable.combineLatest(
     changeWeight$.startWith(70),
     changeHeight$.startWith(170),
@@ -19,20 +23,34 @@ function main(sources) {
     }
   );
 
-  return {
-    DOM: state$.map(state =>
+  return state$;
+}
+
+function view(state$) {
+  const vtree$ = state$.map(state =>
+    div([
       div([
-        div([
-          label(`Weight: ${state.weight}kg`),
-          input('.weight', {type: 'range', min: 40, max: 150, value: state.weight})
-        ]),
-        div([
-          label(`Height: ${state.height}cm`),
-          input('.height', {type: 'range', min: 140, max: 220, value: state.height})
-        ]),
-        h2(`BMI is ${state.bmi}`)
-      ])
-    )
+        label(`Weight: ${state.weight}kg`),
+        input('.weight', {type: 'range', min: 40, max: 150, value: state.weight})
+      ]),
+      div([
+        label(`Height: ${state.height}cm`),
+        input('.height', {type: 'range', min: 140, max: 220, value: state.height})
+      ]),
+      h2(`BMI is ${state.bmi}`)
+    ])
+  );
+
+  return vtree$;
+}
+
+function main(sources) {
+  const {changeWeight$, changeHeight$} = intent(sources.DOM);
+  const state$ = model(changeWeight$, changeHeight$);
+  const vtree$ = view(state$);
+
+  return {
+    DOM: vtree$
   };
 }
 
